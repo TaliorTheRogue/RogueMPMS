2843f3495cead36e40a87571a5fe37cf1a6ab2d8
TaliorTheRogue <donribeiro45@gmail.com>
2025-02-03 12:23:28 +0100

    Initialized project

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>ROGUE MPMS</Text>
+      <Text> Mario Party Map Selector for</Text>
+      <Image 
+        source={require('./assets/Super_Mario_Party_Jamboree_Logo.png')}
+        style={styles.logo}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +21,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    resizeMode: 'stretch',
+    width: 300,
+    height: 200,
+    marginTop: 10,
+    marginBottom: 10,
   },
 });
