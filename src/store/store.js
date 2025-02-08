2de34cd1cd2dd3e3e01af13007d47d1327de65fd
TaliorTheRogue <donribeiro45@gmail.com>
2025-02-08 17:49:18 +0100

    Fixed Repeating Selected Maps

--- src/store/store.js
+++ src/store/store.js
@@ -6,12 +6,17 @@ export const useStore = create((set) => ({
   selectedMap: null,
   mapData: null,
   maps: data,
-  random: () => set(() => {
-    const newId = Math.floor(Math.random() * 7) + 1;
-    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
+  random: () => set((state) => {
+    let newId;
+    do {
+      newId = Math.floor(Math.random() * 7) + 1;
+    } while (newId === state.selectedMap);
+
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+    
     return {
       selectedMap: newId,
-      mapData: data.find(item => Number(item.id) === newId)
+      mapData: data.find(item => Number(item.id) === newId),
     };
   }),
 }));
